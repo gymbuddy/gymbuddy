2d5166c7da1ead5f33529ae2667a3df98ffcd6f0
Brandon <brandonhardy1@gmail.com>
2016-05-02 18:13:13 +0100

    Updated Project

---
+++ ParseStarterProject/ParseStarterProject-Swift-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  ParseStarterProject-Swift-Bridging-Header.h
+//  ParseStarterProject-Swift
+//
+//  Created by Brandon Hardy on 01/05/2016.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+#ifndef ParseStarterProject_Swift_Bridging_Header_h
+#define ParseStarterProject_Swift_Bridging_Header_h
+
+#import <Parse/Parse.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+
+#endif /* ParseStarterProject_Swift_Bridging_Header_h */
---
+++ ParseStarterProject/SignUpViewController.swift
@@ -0,0 +1,60 @@
+import UIKit
+import Parse
+
+class SignUpViewController: UIViewController {
+    
+    @IBAction func signUp(sender: AnyObject) {
+        
+        PFUser.currentUser()?["interestedInFemale"] = interestedInFemale.on
+        try! PFUser.currentUser()?.save()
+    }
+
+    @IBOutlet var userImage: UIImageView!
+    
+    @IBOutlet var interestedInFemale: UISwitch!
+    
+    @IBAction func logOut(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, gender, email"])
+        graphRequest.startWithCompletionHandler { (connection, results, error) -> Void in
+            
+            if error != nil {
+                print(error)
+            } else if let result = results {
+                PFUser.currentUser()?["gender"] = result["gender"]!
+                PFUser.currentUser()?["name"] = result["name"]!
+                
+                do {
+                    try PFUser.currentUser()?.save()
+                } catch let ex {
+                    print(ex)
+                }
+                
+                let userId = result["id"]! as! String
+                let facebookProfilePictureUrl = "https://graph.facebook.com/" + userId + "/picture?type=large"
+                
+                if let fbpicUrl = NSURL(string: facebookProfilePictureUrl),
+                    data = NSData(contentsOfURL: fbpicUrl) {
+                    self.userImage.image = UIImage(data: data)
+                    let imageFile:PFFile = PFFile(data: data)!
+                    PFUser.currentUser()?["image"] = imageFile
+                    do {
+                        try PFUser.currentUser()?.save()
+                    } catch let ex {
+                        print(ex)
+                    }
+                }
+            }
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
